40d8041e46b743f0952d625de2413d49f51ed3b5
Michael MACHADO <michael.machado.reino@gmail.com>
2024-03-20 18:31:54 +0100

    fix key length in for loop to scaleway_domain_record ressource

--- record/main.tf
+++ record/main.tf
@@ -1,5 +1,5 @@
 resource "scaleway_domain_record" "domain_record" {
-  for_each = { for domain_record in var.domain_records : "${domain_record.name}_${domain_record.type}_${domain_record.data}" => domain_record }
+  for_each = { for domain_record in var.domain_records : "${domain_record.name}_${domain_record.type}_${substr(domain_record.data, 0, 8)}" => domain_record }
   dns_zone = each.value.dns_zone
   name     = each.value.name
   type     = each.value.type
